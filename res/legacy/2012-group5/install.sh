9e31d5fc26a7a40b587303ae3e9cf2bc32a54f75
c-w <clemens.wolff+github@gmail.com>
2013-01-23 17:08:53 +0000

    Add reference code: Group5 2012 (Vision)
    This group's vision code actually compiles! Also: it seems fairly well
    done and shouldn't be too hard to tweak.
    
    Added compiled code for DICE. In case this doesn't work out of the box:
    $ cd res/legacy/2012-group5/
    $ rm -rf runVision.sh src/
    $ ./install.sh

---
+++ res/legacy/2012-group5/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# installs 2012/Group5's project
+
+git clone git://github.com/sdp-5-2012/sdp-5-2012.git
+group52012_dir=sdp-5-2012
+cd $group52012_dir
+cp -r src/JavaVision/constants ..
+chmod +x compile.sh
+mkdir -p ./bin
+./compile.sh
+libs=$(ls ./lib/*.jar)
+libs=${libs//[[:space:]]/:}
+{
+echo '#!/bin/bash'
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib'
+echo "java -Djava.library.path=./lib -cp $libs:./bin JavaVision.RunVision"
+} > runVision.sh
+chmod +x runVision.sh
+cd ..
+container_dir=src
+mkdir -p $container_dir
+mv constants/ $group52012_dir/ $container_dir/
+{
+echo '#!/bin/bash'
+echo "cd $container_dir/$group52012_dir"
+echo "./runVision.sh"
+} > runVision.sh
+chmod +x runVision.sh
