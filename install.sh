1059f9cf1494b4d472741904cb5cff3112f18d8e
c-w <clemens.wolff+github@gmail.com>
2013-03-03 10:39:45 +0000

    Add pygame to installer

--- install.sh
+++ install.sh
@@ -68,46 +68,6 @@ function install_pyzmq {
     && rm -rf $lEXTRACT_DIR
 }
 
-
-function install_jbullet {
-    local lLOCATION="`mktemp /tmp/jbullet.XXXXXX.zip`"
-    local lEXTRACT_DIR="`mktemp -d /tmp/jbullet_extracted.XXXXXX`"
-    echo "> Downloading jbullet to $lLOCATION"
-    wget -O "$lLOCATION" "http://jbullet.advel.cz/download/jbullet-20101010.zip" \
-    && echo "> extracting $lLOCATION" \
-    && cd $lEXTRACT_DIR \
-    && unzip $lLOCATION \
-    && cp -R $lEXTRACT_DIR/jbullet-20101010/lib/* $LIB \
-    && rm $lLOCATION \
-    && rm -rf $lEXTRACT_DIR
-}
-
-function install_jbox2d {
-    local lLOCATION="`mktemp /tmp/jbox2d.XXXXXX.zip`"
-    local lEXTRACT_DIR="`mktemp -d /tmp/jbox2d_extracted.XXXXXX`"
-    echo "> Downloading jbox2d to $lLOCATION"
-    wget -O "$lLOCATION" "http://jbox2d.googlecode.com/files/jbox2d-2.1.2.1.zip" \
-    && echo "> extracting $lLOCATION" \
-    && cd $lEXTRACT_DIR \
-    && unzip $lLOCATION \
-    && cp $lEXTRACT_DIR/jbox2d-library/target/jbox2d-library-2.1.2.1-SNAPSHOT-jar-with-dependencies.jar $LIB \
-    && cp $lEXTRACT_DIR/jbox2d-testbed/target/jbox2d-testbed-2.1.2.1-SNAPSHOT-jar-with-dependencies.jar $LIB \
-    && rm $lLOCATION \
-    && rm -rf $lEXTRACT_DIR
-}
-
-function install_slf4j {
-   local lLOCATION="`mktemp /tmp/slf4j.XXXXXXX.tar.gz`"
-   local lEXTRACT_DIR="`mktemp -d /tmp/slf4j_extracted.XXXXXX`"
-   wget -O "$lLOCATION" "http://www.slf4j.org/dist/slf4j-1.6.4.tar.gz" \
-   && echo "> extractiong $lLOCATION" \
-   && cd $lEXTRACT_DIR \
-   && tar xvf $lLOCATION \
-   && cp "$lEXTRACT_DIR/slf4j-1.6.4/slf4j-api-1.6.4.jar" $LIB \
-   && rm $lLOCATION \
-   && rm -rf $lEXTRACT_DIR
-}
-
 function cleanup {
     rm -rf $LIB 
     mkdir -p $LIB
@@ -115,6 +75,7 @@ function cleanup {
     STR="export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$LD_LIBRARY_DIR"
     add_to_bashrc $STR
 }
+
 function install_bluetooth {
     local lTMP_BLUETOOTH_LIB="`mktemp /tmp/libbluetooth.XXXXX.tar.gz`"
     # Currently this is my dropbox acc public folder -- do not want to burden the repo with binaries -- Saulius  
@@ -125,44 +86,29 @@ function install_bluetooth {
     && cd $LIB && tar xvf $lTMP_BLUETOOTH_LIB && cd $SDP_HOME && rm $lTMP_BLUETOOTH_LIB
 }
 
-function install_mockito {
-    wget "http://mockito.googlecode.com/files/mockito-all-1.9.0.jar" -O "$LIB/mockito-all-1.9.0.jar"
-}
-
-function install_joptsimple {
-    wget -U "SomeUserAgent/1.0" "http://repo1.maven.org/maven2/net/sf/jopt-simple/jopt-simple/4.3/jopt-simple-4.3.jar" -O "$LIB/jopt-simple-4.3.jar"
-}
-
-function install_reflections {
-    local lTMP_REFLECTIONS_INSTALL_DIR="`mktemp -d /tmp/reflections.XXXXX`"
-    wget "http://reflections.googlecode.com/files/reflections-0.9.5.one-jar.jar" -O "$lTMP_REFLECTIONS_INSTALL_DIR/reflections-0.9.5.one-jar.jar" \
-    && cd $lTMP_REFLECTIONS_INSTALL_DIR \
-    && jar xf reflections-0.9.5.one-jar.jar \
-    && cp reflections-0.9.5.jar $LIB \
-    && rm -f $lTMP_REFLECTIONS_INSTALL_DIR/lib/slf4j* \
-    && cp lib/* $LIB \
-    && rm -rf $lTMP_REFLECTIONS_INSTALL_DIR
-}
-
-function install_neuroph {
-    local lTMP_NEUROPH="`mktemp -d /tmp/neuroph.XXXXX`"
-    wget -O "$lTMP_NEUROPH/neuroph-2.6.zip"  "http://sourceforge.net/projects/neuroph/files/neuroph%202.6/neuroph-2.6.zip/download"
-    unzip "$lTMP_NEUROPH/neuroph-2.6.zip" -d $LIB
+function install_pygame {
+    local lLOCATION="`mktemp /tmp/pygame.XXXXXX.tar.gz`"
+    local lEXTRACT_DIR="`mktemp -d /tmp/pygame_extracted.XXXXXX`"
+    echo "> Downloading pygame to $lLOCATION"
+    wget -O "$lLOCATION" "http://www.pygame.org/ftp/pygame-1.9.1release.tar.gz" \
+    && echo "> extracting $lLOCATION" \
+    && cd $lEXTRACT_DIR \
+    && tar xvf $lLOCATION \
+    && cd pygame-1.9.1release \
+    && python setup.py install --prefix=$LIB\
+    && STR="export PYTHONPATH=\$PYTHONPATH:$LIB/lib64/python2.6/site-packages/" \
+    && add_to_bashrc $STR \
+    && rm $lLOCATION \
+    && rm -rf $lEXTRACT_DIR
 }
 
 cleanup
+
 install_lejos
 install_bluetooth
 install_zmq
 install_jzmq
 install_pyzmq
-
-#install_slf4j # Seems to be required for the physics demo and reflections
-#install_jbullet
-#install_jbox2d
-#install_mockito   # Mocking framework -- used in some JUnit tests
-#install_joptsimple # Simple arguments parser
-#install_reflections # For processing @Annotations
-#install_neuroph # For bezier neural network controller
+install_pygame
 
 source ~/.bashrc
--- tools/motion_vectors/motion_vectors.sh
+++ tools/motion_vectors/motion_vectors.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 base_dir=$(dirname $(readlink -f $0))
-lib_dir=$base_dir/lib
 src_dir=$base_dir/src
-
-# install pygame locally if necessary
-PYTHONPATH=$lib_dir:$PYTHONPATH
-export PYTHONPATH
-python -c "import pygame" &> /dev/null
-if [ $? -gt 0 ]; then
-    mkdir -p $lib_dir
-    easy_install --install-dir $lib_dir pygame
-fi
-
-# run script
 cd $src_dir
 python $(basename $0 .sh).py $@
