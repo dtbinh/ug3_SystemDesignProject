0fb779bccc8770a700ed7fb98381397b44715df5
c-w <clemens.wolff+github@gmail.com>
2013-03-08 19:23:20 +0000

    Remove unused parts of source

--- src/examples/nxt/HelloWorld/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CLASS=HelloWorld
-
-nxjc $CLASS.java && \
-nxjlink -o $CLASS.nxj $CLASS && \
-nxjupload -r $CLASS.nxj
--- src/examples/nxt/i2c/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CLASS=i2c
-
-nxjc $CLASS.java && \
-nxjlink -o $CLASS.nxj $CLASS && \
-nxjupload -r $CLASS.nxj
