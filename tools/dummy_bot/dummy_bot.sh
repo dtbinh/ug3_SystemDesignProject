e005f545132030a92ff7d98919b65620b015a22c
c-w <clemens.wolff+github@gmail.com>
2013-03-09 21:07:27 +0000

    Tools set PYTHONPATH if needed

--- tools/dummy_bot/dummy_bot.sh
+++ tools/dummy_bot/dummy_bot.sh
@@ -2,5 +2,10 @@
 
 base_dir=$(dirname $(readlink -f $0))
 src_dir=$base_dir/src
+lib_dir=$(readlink -f "$base_dir/../../lib/lib64/python2.6/site-packages")
+python -c "import pygame; import zmq" &> /dev/null
+if [ $? -gt 0 ]; then
+    export PYTHONPATH="$lib_dir:$PYTHONPATH"
+fi
 cd $src_dir
 python $(basename $0 .sh).py $@
