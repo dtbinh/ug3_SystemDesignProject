09164137fefb866b5557ea479e71d4da3d84291f
c-w <clemens.wolff+github@gmail.com>
2013-03-09 20:27:54 +0000

    Remove all Eclipse red flags

--- src/Planning/CommandStack.java
+++ src/Planning/CommandStack.java
@@ -3,7 +3,7 @@ package Planning;
 import JavaVision.Position;
 import java.awt.Point;
 import java.util.List;
-import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Holds the actions the robot wants to perform
@@ -11,17 +11,43 @@ import java.util.LinkedList;
  * @author c-w
  */
 public class CommandStack {
-	private LinkedList<Command> stack;
+	private Stack<Command> stack;
 
 	public CommandStack() {
-		stack = new LinkedList<Command>();
+		stack = new Stack<Command>();
+	}
+
+	public void pushMoveCommand(List<Point> points) {
+		int numel = points.size();
+		Position lastPosition = new Position(points.get(numel - 1));
+		// Skip first element of list (current robot coordinates) - we're there already
+		for (int i = numel - 1; i > 0; i--) {
+			this.pushMoveCommand(new Position(points.get(i)), lastPosition, i == 1);
+		}
+	}
+
+	public void pushMoveCommand(Position movePoint, Position rotatePoint,
+								boolean hardRotate) {
+		this.push(new MoveCommand(movePoint, rotatePoint, hardRotate));
+	}
+
+	public void pushKickCommand(Position kickPoint, Position ballPoint) {
+		KickCommand kickCommand = new KickCommand();
+		if (!this.contains(kickCommand)) {
+			this.push(kickCommand);
+			this.push(new MoveCommand(kickPoint, ballPoint, true));
+		}
+	}
+
+	public void push(Command c) {
+		stack.push(c);
 	}
 
 	public Command getFirst() {
 		if (stack.isEmpty()) {
 			return null ;
 		} else {
-			return stack.getFirst();
+			return stack.peek();
 		}
 	}
 
@@ -32,31 +58,9 @@ public class CommandStack {
 	public void clear() {
 		stack.clear();
 	}
-	
-	public void pushMoveCommand(List<Point> points) {
-		int numel = points.size();
-		Position lastPosition = new Position(points.get(numel - 1));
-		// Skip first element of list (current robot coordinates) - we're there already
-		for (int i = numel - 1; i > 0; i--) {
-			pushMoveCommand(new Position(points.get(i)), lastPosition, i == 1);
-		}
-	}
 
-	public void pushMoveCommand(Position movePoint, Position rotatePoint,
-								boolean hardRotate) {
-		stack.push(new MoveCommand(movePoint, rotatePoint, hardRotate));
-		//while (stack.size()> 10){
-		//	stack.remove(10);
-		//}
-		//cleaned the stack to avoid memory issues.
-	}
-	
-	public void pushKickCommand(Position kickPoint, Position ballPoint) {
-		KickCommand kickCommand = new KickCommand();
-		if (!stack.contains(kickCommand)) {
-			stack.push(kickCommand);
-			stack.push(new MoveCommand(kickPoint, ballPoint, true));
-		}
+	public boolean contains(Command c) {
+		return stack.search(c) != -1;
 	}
 
 	public boolean isEmpty() {
