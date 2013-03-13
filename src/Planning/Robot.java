a94ee634391fe4848a9a31ed5db425ca69968c0a
Jona Duka <s0930891@cumbrae.inf.ed.ac.uk>
2013-03-13 02:09:01 +0000

    new constructors

--- src/Planning/Robot.java
+++ src/Planning/Robot.java
@@ -1,4 +1,15 @@
 package Planning;
 
+import JavaVision.Position;
+
 public class Robot extends ObjectDetails {
+	
+	public Robot() {
+		
+	}
+	
+	public Robot (Position coors, float angle) {
+		this.setCoors(coors);
+		this.setAngle(angle);
+	}
 }
